3df26db7227b76b7e5a73dc9aafd672a2a5c7fe3
triet <pvmtriet232@gmail.com>
2023-04-02 20:38:07 +0700

    add main.tf with eks_iam_role

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "eks" {
+  name = "eks-main-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}

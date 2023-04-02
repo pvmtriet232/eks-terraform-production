905877c1c0a4b594403d8096ca883419a8e5c3e6
triet <pvmtriet232@gmail.com>
2023-04-02 20:55:59 +0700

    add aws_iam_role...

---
+++ aws_iam_role_policy_attachment.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_role_policy_attachment" "main-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_iam_role_policy_attachment" "main-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks.name
+}
